4da067bbcfa9ae38753175b9fe628c86a2bc864d
Rajib Ganguly <rajib.ganguly@v2solutions.com>
2024-01-25 19:08:50 +0530

    added changes

--- server.js
+++ server.js
@@ -6,16 +6,24 @@ const app = express(),
       cors = require('cors')
       port = 8080;
 
+const { v4: uuidv4 } = require('uuid');
+
 
 // place holder for the data json
 // var users = require('./db/data.json');
 var books = require('./db/booklist.json');
 var loginUsers = require('./db/login.json');
+var tasks = require('./db/tasks.json');
 
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, '../colab/build')));
+app.use(express.static(path.join(__dirname, './reactrgapp/build/')));
+
+app.get('/', (req,res) => {
+  res.sendFile('./reactrgapp/build/index.html')
+  res.sendFile(path.join(__dirname, './reactrgapp/build/index.html'));
+});
 
 app.get('/api/books', (req, res) => {
   fs.readFile("./db/booklist.json", function(err, data) {       
@@ -58,6 +66,8 @@ app.post('/login', (req, res) => {
     res.json("Login success!");  
 });
 
+
+
 app.patch('/updatebook', (req, res) => {
   fs.readFile("./db/booklist.json", function(err, data) {       
     // Check for errors 
@@ -79,9 +89,60 @@ app.patch('/updatebook', (req, res) => {
     res.json("updated records");  
 });
 
-app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname, './reactrgapp/build/index.html'));
+// TASK RELATED
+
+app.post('/admin/requestjob', (req, res) => {  
+  fs.readFile("./db/tasks.json", function(err, data) {       
+    // Check for errors 
+    if (err) throw err; 
+    tasks = JSON.parse(data); 
+  });
+  const requestNew = req.body;
+  requestNew['id'] = uuidv4();
+
+  tasks.push(requestNew)
+  console.log(tasks)
+
+  fs.writeFile("./db/tasks.json", JSON.stringify(tasks), err => { 
+    // Checking for errors 
+    if (err) throw err; 
+  });
+  res.json(tasks); 
+})
+
+app.delete('/admin/requestjob/:id', (req, res) => {  
+  fs.readFile("./db/tasks.json", function(err, data) {       
+    // Check for errors 
+    if (err) throw err; 
+    tasks = JSON.parse(data); 
+  });
+  const idToRemove = id;
+
+  const requestNew = req.body;
+  const finalTaskArray = requestNew.filter(obj => obj.id !== idToRemove);
+
+  fs.writeFile("./db/tasks.json", JSON.stringify(finalTaskArray), err => { 
+    // Checking for errors 
+    if (err) throw err; 
+  });
+  res.json(finalTaskArray); 
+})
+
+
+
+
+app.get('/admin/requestjob', async (req, res) => {
+  fs.readFile("./db/tasks.json", function(err, data) {       
+    // Check for errors 
+    if (err) throw err;    
+    // Converting to JSON 
+    tasks = JSON.parse(data); 
+  });
+  //const booksObj = await tasks.filter((ts) =>  ts.id == req.params.id)
+  res.json(tasks);
 });
+
+
 
 app.listen(port, () => {
     console.log(`Server listening on the port::${port}`);
